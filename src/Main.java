c5e11325a2bd66942c04734f3251533a97036919
Navya Gupta <navyag0509@gmail.com>
2026-01-15 13:29:58 -0500

    Created PersonReader.java, and ProductWriter.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
 
     }
-}
